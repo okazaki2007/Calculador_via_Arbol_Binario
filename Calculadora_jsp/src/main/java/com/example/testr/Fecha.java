31bdb9da5939c26a69a6c6d583fa708d370af396
okazaki2007 <86208462+okazaki2007@users.noreply.github.com>
2021-11-11 09:15:10 -0600

    Actualizacion de variable

--- Calculadora_jsp/src/main/java/com/example/testr/Fecha.java
+++ Calculadora_jsp/src/main/java/com/example/testr/Fecha.java
@@ -1,18 +1,32 @@
-package com.example.testr;
+package com.example.calculadora_final;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+
+/**
+ *Clase para obtener parametros de fecha y hora
+ * @author  Jeff
+ */
 
 public class Fecha {
 
 
+    /**
+     * Metodo para devolver fecha actual
+     * @return fecha local
+     */
+
+
     public String getFormatoFecha(){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyy");
         return dtf.format(LocalDateTime.now());
 
     }
+
+    /**
+     * Metodo para devolver hora actual
+     * @return hora local
+     */
 
     public String getFormatoHora(){
         DateTimeFormatter dth = DateTimeFormatter.ofPattern("HH:mm:ss");
